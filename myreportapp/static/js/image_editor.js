f01ecbbf1440d96b2b4155a64c24ded2cc03a364
Marcos-Capristo <marcos.capristo@gmail.com>
2025-01-01 08:31:20 -0300

    ajustes no zoom

--- myreportapp/static/js/image_editor.js
+++ myreportapp/static/js/image_editor.js
@@ -1,6 +1,3 @@
-// ImageEditor.js
-
-
 export default class ImageEditor {
     constructor(canvasSelector, maxCanvasValue = 800) {
         this.canvas = document.querySelector(canvasSelector);
@@ -12,42 +9,39 @@ export default class ImageEditor {
         this.maxCanvasValue = maxCanvasValue;
         this.maxHistory = 15;
         this.aspectRatio = 1;
-        this.zoom = 1;
+        this.zoomLevel = 1;
         this.realImage = new Image();
+        this.transitionImage = new Image();
         this.tempCanvas = document.createElement('canvas');
         this.tempCtx = this.tempCanvas.getContext('2d', { willReadFrequently: true });
         this.action = 0;
-        this.actions = ['selecionar',
+        this.actions = [
+            'selecionar',
             'rotacionar horário',
             'rotacionar anti-horário',
             'zoom',
-            'recortar',
-        ]
+            'recortar'
+        ];
         this.listOfCanvasImages = [];
         this.listOfVirtualCanvasImages = [];
         this.logOfChanges = [];
     }
 
-
-    // LISTA DE AÇÕES E DADOS EXIBIDOS DURANTE A FASE DE PROJETO.
-    thisLogAction() {
-        const dataUrl = this.virtualCanvas.toDataURL(); // Obtém a imagem otimizada
-        const base64Length = dataUrl.length - 'data:image/jpeg;base64,'.length; // Exclui o prefixo
-        const sizeInBytes = (base64Length * 3) / 4; // Converte de Base64 para bytes
-        this.sizeInMb = sizeInBytes / (1024 * 1024); // Converte de bytes para MB
+    logAction() {
+        const dataUrl = this.virtualCanvas.toDataURL();
+        const base64Length = dataUrl.length - 'data:image/jpeg;base64,'.length;
+        const sizeInBytes = (base64Length * 3) / 4;
+        this.sizeInMb = sizeInBytes / (1024 * 1024);
         console.log(`\nAção: ${this.actions[this.action]}`);
         console.log(`Imagem Original ${this.realImage.width} x ${this.realImage.height}`);
         console.log(`Imagem no canvas: ${this.canvas.width} x ${this.canvas.height}`);
-        console.log(`Imagem no canvas virutal: ${this.virtualCanvas.width} x ${this.virtualCanvas.height}`);
-        console.log(`Tamanho em Mb do arquivo de impressão: ${this.sizeInMb.toFixed(2)} MB`);
-        console.log(`Lista de ações: ${this.logOfChanges}`)
-
+        console.log(`Imagem no canvas virtual: ${this.virtualCanvas.width} x ${this.virtualCanvas.height}`);
+        console.log(`Tamanho em MB do arquivo de impressão: ${this.sizeInMb.toFixed(2)} MB`);
+        console.log(`Histórico de ações: ${this.logOfChanges}`);
     }
 
-
-    // AJUSTA AS DIMENSÕES DOS CANVAS, SE RETRATO OU PAISAGEM
     adjustSize(image) {
-        this.aspectRatio = image.width / image.height
+        this.aspectRatio = image.width / image.height;
         if (image.width > image.height) {
             this.canvas.width = this.maxCanvasValue;
             this.canvas.height = this.maxCanvasValue / this.aspectRatio;
@@ -57,11 +51,8 @@ export default class ImageEditor {
         }
         this.virtualCanvas.width = this.factorCanvas * this.canvas.width;
         this.virtualCanvas.height = this.factorCanvas * this.canvas.height;
-        //this.thisLogAction();
     }
 
-
-    // SELECIONA UMA IMAGEM NO EXPLORADOR, EXIBE NO CANVAS E ATRIBUI AO CANVAS VIRTUAL
     selectImage() {
         const input = document.createElement('input');
         this.action = 0;
@@ -76,42 +67,18 @@ export default class ImageEditor {
                 this.realImage.onload = () => {
                     this.adjustSize(this.realImage);
 
-                    // Desenhar no canvas principal
-                    this.ctx.drawImage(
-                        this.realImage,
-                        0, 0,
-                        this.canvas.width,
-                        this.canvas.height
-                    );
+                    this.ctx.drawImage(this.realImage, 0, 0, this.canvas.width, this.canvas.height);
+                    this.virtualCtx.drawImage(this.realImage, 0, 0, this.virtualCanvas.width, this.virtualCanvas.height);
 
-                    // Desenhar no canvas virtual
-                    this.virtualCtx.drawImage(
-                        this.realImage,
-                        0, 0,
-                        this.virtualCanvas.width,
-                        this.virtualCanvas.height
-                    );
-
-                    // Gerar URL da imagem com qualidade reduzida
                     const reducedQualityImageURL = this.virtualCanvas.toDataURL('image/jpeg', 0.95);
-
-                    // Criar uma nova imagem para carregar a URL de qualidade reduzida
                     const reducedQualityImage = new Image();
                     reducedQualityImage.src = reducedQualityImageURL;
 
                     reducedQualityImage.onload = () => {
-                        // Limpar o canvas virtual e desenhar a imagem otimizada
                         this.virtualCtx.clearRect(0, 0, this.virtualCanvas.width, this.virtualCanvas.height);
-                        this.virtualCtx.drawImage(
-                            reducedQualityImage,
-                            0, 0,
-                            this.virtualCanvas.width,
-                            this.virtualCanvas.height
-                        );
-                        //this.thisLogAction();
-                        //console.log(`Tamanho em Mb do arquivo de impressão: ${this.sizeInMb.toFixed(2)} MB`);                    // Salvar o estado inicial
+                        this.virtualCtx.drawImage(reducedQualityImage, 0, 0, this.virtualCanvas.width, this.virtualCanvas.height);
                         this.saveState();
-                        this.thisLogAction();
+                        this.logAction();
                     };
                 };
             };
@@ -120,70 +87,169 @@ export default class ImageEditor {
         input.click();
     }
 
+    rotate(canvas, ctx, direction = 1) {
+        this.tempCanvas.width = canvas.width;
+        this.tempCanvas.height = canvas.height;
+        this.tempCtx.drawImage(canvas, 0, 0);
 
+        const newWidth = canvas.height;
+        const newHeight = canvas.width;
+        canvas.width = newWidth;
+        canvas.height = newHeight;
 
-    // ROTACIONA  A IMAGEM
-    rotate(element_canvas, element_ctx, clock_direction = 1) {
-        this.tempCanvas.width = element_canvas.width;
-        this.tempCanvas.height = element_canvas.height;
-        this.tempCtx.drawImage(element_canvas, 0, 0);    
-        const newWidth = element_canvas.height;
-        const newHeight = element_canvas.width;    
-        element_canvas.width = newWidth;
-        element_canvas.height = newHeight;    
-        element_ctx.clearRect(0, 0, newWidth, newHeight);
-        element_ctx.save();
-        element_ctx.translate(newWidth / 2, newHeight / 2);
-        element_ctx.rotate(clock_direction * Math.PI / 2); // 90 graus ou -90 graus
-        element_ctx.drawImage(this.tempCanvas, -this.tempCanvas.width / 2, -this.tempCanvas.height / 2);
-        element_ctx.restore();    
-        //this.thisLogAction(); // Verificar estado do canvas
-        console.log(`Tamanho em Mb do arquivo de impressão: ${this.sizeInMb.toFixed(2)} MB`);
+        ctx.clearRect(0, 0, newWidth, newHeight);
+        ctx.save();
+        ctx.translate(newWidth / 2, newHeight / 2);
+        ctx.rotate(direction * Math.PI / 2);
+        ctx.drawImage(this.tempCanvas, -this.tempCanvas.width / 2, -this.tempCanvas.height / 2);
+        ctx.restore();
     }
-    
 
-    // ROTACIONA EM SENTIDO HORÁRIO
     rotateClockwise() {
         this.action = 1;
         this.rotate(this.canvas, this.ctx);
         this.rotate(this.virtualCanvas, this.virtualCtx);
         this.saveState();
-        this.thisLogAction();
+        this.logAction();
     }
-    
 
-    // ROTAICONA EM SENTIDO ANTI-HORÁRIO
     rotateCounterClockwise() {
         this.action = 2;
         this.rotate(this.canvas, this.ctx, -1);
         this.rotate(this.virtualCanvas, this.virtualCtx, -1);
         this.saveState();
-        this.thisLogAction();
+        this.logAction();
+    }
+
+    extractImage(canvas) {
+        if (!(canvas instanceof HTMLCanvasElement)) {
+            throw new Error("O parâmetro fornecido não é um canvas válido.");
+        }
+        const imageDataURL = canvas.toDataURL();
+        const image = new Image();
+        image.src = imageDataURL;
+        return image;
     }
     
+    // O código funciona quase perfeito, mas aplica um recorte nas imagens ao aplicar zoomIn, visto que
+    // o usuário pode querer aplicar um zoomOut logo em seguida.
+    zoom(factor) {
+        // Atribuição da imagem do this.virtualCanvas porque a qualidade é melhor doque a do this.canvas.
+        // transitionImage será aplicada primeiramente ao canvas virtual e depois ao canvas visível,
+        // mantendo uma boa qualidade de impressão e visualização.
+        this.transitionImage = this.extractImage(this.virtualCanvas);
+        this.transitionImage.onload = () => {
 
-    // ACRESCENTA AS IMAGEMS DO CANVAS E DO VIRTUALCANVAS ÀS SUAS REPECTIVAS LISTAS
+            // Calcula as novas dimensões do canvas virtual
+            let newWidth = this.virtualCanvas.width * factor;
+            let newHeight = this.virtualCanvas.height * factor;    
+            // Calcula os deslocamentos para centralizar
+            let left = (newWidth - this.virtualCanvas.width) / 2;
+            let top = (newHeight - this.virtualCanvas.height) / 2;    
+            // Limpa o canvas
+            this.virtualCtx.clearRect(0, 0, this.virtualCanvas.width, this.virtualCanvas.height);    
+            // Redesenha a imagem no canvas com as novas dimensões
+            // Mas aqui a imgem é recortada pelas arestas do canvas.
+            // Dessa forma, ao aplicar zoomout após essa operação, a imagem ficará truncada.
+            this.virtualCtx.drawImage(
+                this.transitionImage, // Imagem a ser desenhada
+                -left,                // Coordenada X de início
+                -top,                 // Coordenada Y de início
+                newWidth,             // Largura da imagem ajustada
+                newHeight             // Altura da imagem ajustada
+            );
+
+
+            // Calcula as novas dimensões do canvas visivel
+            // O objetivo é que todas as alterações feitas no canvas virtual sejam visiveis
+            // ao usuário na canvas que aparece na tela.
+            newWidth = this.canvas.width * factor;
+            newHeight = this.canvas.height * factor;    
+            // Calcula os deslocamentos para centralizar
+            left = (newWidth - this.canvas.width) / 2;
+            top = (newHeight - this.canvas.height) / 2;    
+            // Limpa o canvas
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);    
+            // Redesenha a imagem no canvas com as novas dimensões
+            // mesmo problema do exposto acima no canvas virtual.
+            this.ctx.drawImage(
+                this.transitionImage, // Imagem a ser desenhada
+                -left,                // Coordenada X de início
+                -top,                 // Coordenada Y de início
+                newWidth,             // Largura da imagem ajustada
+                newHeight             // Altura da imagem ajustada
+            );
+    
+            // Logs para depuração
+            console.log(`Tamanho do canvas: ${this.canvas.width} x ${this.canvas.height}`);
+            console.log(`Tamanho da imagem: ${this.transitionImage.width} x ${this.transitionImage.height}`); // Isso demonstra que a imgem está sendo recortada, o que não é esperado.
+        };
+    };
+    
+    
+/*
+    zoom1(factor) {
+        this.zoomLevel += factor;
+        if (this.zoomLevel < 0.1) this.zoomLevel = 0.1;
+    
+        const canvasWidth = this.canvas.width;
+        const canvasHeight = this.canvas.height;
+    
+        const virtualWidth = this.virtualCanvas.width * this.zoomLevel;
+        const virtualHeight = this.virtualCanvas.height * this.zoomLevel;
+    
+        // Calcula o deslocamento
+        const offsetX = (virtualWidth - this.virtualCanvas.width) / 2;
+        const offsetY = (virtualHeight - this.virtualCanvas.height) / 2;
+    
+        this.ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+        this.virtualCtx.clearRect(0, 0, this.virtualCanvas.width, this.virtualCanvas.height);
+    
+        // Aplica o deslocamento ao desenhar na virtualCanvas
+        this.virtualCtx.save();
+        this.virtualCtx.translate(-offsetX, -offsetY);
+        this.virtualCtx.scale(this.zoomLevel, this.zoomLevel);
+        this.virtualCtx.drawImage(this.realImage, 0, 0);
+        this.virtualCtx.restore();
+    
+        // Ajusta o deslocamento na canvas principal
+        this.ctx.drawImage(this.virtualCanvas, 0, 0, canvasWidth, canvasHeight);
+    
+        this.logAction();
+    }
+
+*/
+    zoomIn() {
+        this.action = 3;
+        this.zoom(0.1);
+    }
+
+    zoomOut() {
+        const factor = -0.1;
+        this.canvas.width *= factor;
+        this.canvas.height *= factor;
+        this.action = 4;
+        this.zoom(factor);
+    }
+
     saveState() {
         if (this.listOfCanvasImages.length >= this.maxHistory) {
             this.listOfCanvasImages.shift();
         }
         this.listOfCanvasImages.push(this.canvas.toDataURL('image/jpeg', 0.9));
+
         if (this.listOfVirtualCanvasImages.length >= this.maxHistory) {
             this.listOfVirtualCanvasImages.shift();
         }
         this.listOfVirtualCanvasImages.push(this.virtualCanvas.toDataURL('image/jpeg', 0.9));
-        if (this.logOfChanges.length >= this.maxHistory){
+
+        if (this.logOfChanges.length >= this.maxHistory) {
             this.logOfChanges.shift();
         }
         this.logOfChanges.push(this.action);
-        console.log(`Estado salvo: canvas principal e virtual (${this.listOfCanvasImages.length} estados armazenados).`);
     }
 
-
-    // DESFAZ A ÚLTIMA OPERAÇÃO
     undo() {
-        alert('desfazer');
-        console.log(`Tamanho em Mb do arquivo de impressão: ${this.sizeInMb.toFixed(2)} MB`);
+        console.log('Função desfazer em desenvolvimento.');
     }
-
 }
