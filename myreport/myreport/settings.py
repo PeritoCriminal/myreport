1f9673491fc416ab84a221df74c92a2375f0fe2c
Marcos-Capristo <marcos.capristo@gmail.com>
2025-12-05 21:45:42 -0300

    ajustado modo DEBUG para rodar no render e na máquina local.

--- myreport/myreport/settings.py
+++ myreport/myreport/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -23,9 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6f7-s-0=2hwe%pyd+al81e3f13a)ub2c2my%tr$*o7)o)p$byq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'myreport-3xzv.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
