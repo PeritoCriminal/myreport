d0cc019813b300c400d65267d148e38ae9fbacd4
Marcos-Capristo <marcos.capristo@gmail.com>
2026-02-25 18:12:04 -0300

    refacto(__init__.py): Limpar imports desnecessários.

--- myreport/report_maker/views/__init__.py
+++ myreport/report_maker/views/__init__.py
@@ -1,8 +1,1 @@
-from .report_case import *
-from .generic_object import *
-from .images import *
-from .report_text_block import *
-from .report_outline import *
-from .report_pdf_generator import *
-from .ai_textblock import *
-from .exam_cadaver import *
+# path: myreport/report_maker/views/__init__.py
